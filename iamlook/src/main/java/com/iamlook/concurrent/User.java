a39b4e584ef6a85a0a762e39696443853d4108ac
youxun <632017924@qq.com>
2019-02-22 13:41:52 +0800

    init

---
+++ iamlook/src/main/java/com/iamlook/concurrent/User.java
@@ -0,0 +1,30 @@
+package com.iamlook.concurrent;
+
+/**
+ * @program: notes
+ * @description:
+ * @author: Created by youxun
+ * @create: 2019-02-22 11:37
+ **/
+public class User {
+
+    private String name;
+
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
