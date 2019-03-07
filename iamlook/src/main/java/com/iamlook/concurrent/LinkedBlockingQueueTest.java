24b26845339bdf5a93531cf21f4104dce55cca11
youxun <632017924@qq.com>
2019-03-07 15:05:45 +0800

    test

--- iamlook/src/main/java/com/iamlook/concurrent/LinkedBlockingQueueTest.java
+++ iamlook/src/main/java/com/iamlook/concurrent/LinkedBlockingQueueTest.java
@@ -1,9 +1,7 @@
 package com.iamlook.concurrent;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.LinkedList;
+import java.util.concurrent.*;
 
 /**
  * @program: notes
@@ -14,6 +12,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class LinkedBlockingQueueTest {
 
     LinkedBlockingQueue linkedBlockingQueue = new LinkedBlockingQueue();
+    ConcurrentLinkedQueue concurrentLinkedQueue = new ConcurrentLinkedQueue();
 
     public static void main(String[] args) {
         ConcurrentHashMap<String,String> map = new ConcurrentHashMap(); //初始化ConcurrentHashMap
@@ -29,8 +28,25 @@ public class LinkedBlockingQueueTest {
         System.out.println(size);
 
         ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
-        ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
-        ExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
+        /*ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+        ExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);*/
 
+        LinkedBlockingQueueTest test = new LinkedBlockingQueueTest();
+        for(int i = 0; i < 1000; i++){
+            cachedThreadPool.submit(test.new TakePoll());
+        }
+
+    }
+
+
+
+    class TakePoll implements Runnable{
+        @Override
+        public void run() {
+            for(int i =0; i < 1000; i++){
+                linkedBlockingQueue.add(i);
+                System.out.println(linkedBlockingQueue.size());
+            }
+        }
     }
 }
